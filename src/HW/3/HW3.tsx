04ca29fc86f69afe10e8274d3348b20ea2d6f8b2
Roman_cox <rom.cox@gmail.com>
2023-09-15 22:39:02 +0300

    refactor third & forth hw

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -16,7 +16,6 @@ export const HW3 = () => {
   ]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value)
     setCurrentText(event.target.value);
   };
 
@@ -30,7 +29,7 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
         <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
